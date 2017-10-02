e83ddf0f39cb77611a284c2c8db602073668ced0
brickatyourfeet <brickatyourfeet@gmail.com>
2017-10-02 10:39:47 -0700

    add binary search, needs revision

--- binary-search.js
+++ binary-search.js
@@ -5,8 +5,8 @@
 function binarySearch(numArray, key){
     var midIndex = Math.floor(numArray.length / 2)
     var midElement = numArray[midIndex]
-    
-    if(midElement === key){
+    //have to add the or in the if statement to check for last element
+    if(midElement === key || numArray[numArray.length]){
         return true
     }else if(midElement < key && numArray.length > 1){  //includes length must be more than one because if it were one and didn't get returned by the first check, it will not be found in the array - then the splice returns second half of array into the funciton again
         return binarySearch(numArray.splice(midIndex, numArray.length), key)
@@ -16,7 +16,8 @@ function binarySearch(numArray, key){
     
 }
 
-
-console.log(binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 56))
+console.log(binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 71))
 
 console.log(binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 11))
+
+console.log(binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 5))
